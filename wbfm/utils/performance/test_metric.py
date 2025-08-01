e7b5550530738b18cf7f994cfe1dddf70251dca2
11809646 <e11809646@student.tuwien.ac.at>
2025-08-01 10:19:49 +0200

    rename column of one DF

--- wbfm/utils/performance/test_metric.py
+++ wbfm/utils/performance/test_metric.py
@@ -1,5 +1,6 @@
 from wbfm.utils.projects.finished_project_data import ProjectData
 from wbfm.utils.general.high_performance_pandas import get_names_from_df
+from wbfm.utils.neuron_matching.utils_candidate_matches import rename_columns_using_matching
 
 
 from wbfm.utils.performance.comparing_ground_truth import calculate_accuracy_from_dataframes, calc_accuracy_of_pipeline_steps
@@ -17,21 +18,18 @@ project_data_res = ProjectData.load_final_project_data(fname_res)
 
 df_res = project_data_res.final_tracks
 df_gt = project_data_gt.final_tracks
-df_res = df_res.xs('raw_segmentation_id', axis=1, level=1)
-df_gt = df_gt.xs('raw_segmentation_id', axis=1, level=1)
+"""df_res = df_res.xs('raw_segmentation_id', axis=1, level=1)
+df_gt = df_gt.xs('raw_segmentation_id', axis=1, level=1)"""
 
-df_gt = df_gt.iloc[1:1599]
-
-
+#df_gt = df_gt.iloc[1:1599]
 
 
-print(df_res)
+df_res_renamed = rename_columns_using_matching(df_gt, df_res, column='raw_segmentation_id')
+
+print(df_res_renamed)
 print(df_gt)
-#print(df_gt[sorted(df_gt.columns)[0]])
+
 column_names=['raw_segmentation_id']
-#print(df_res[sorted(df_res.columns.get_level_values(0).unique())[0]])
-#df_acc_pipeline = calculate_accuracy_from_dataframes(df_gt, df_res, column_names)
-#print(df_acc_pipeline)
-#print(df_gt['100906'])
-#print(df_res['neuron_070'])
+df_acc_pipeline = calculate_accuracy_from_dataframes(df_gt, df_res, column_names)
+print(df_acc_pipeline)
 
