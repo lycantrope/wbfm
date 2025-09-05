14923f9fe2ededa6a4886a25ea9b22831db67914
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2025-09-05 10:50:52 +0200

    Check for inconsistent usage of superglue and barlow; #936

--- wbfm/utils/neuron_matching/class_frame_pair.py
+++ wbfm/utils/neuron_matching/class_frame_pair.py
@@ -92,6 +92,10 @@ class FramePairOptions:
         pairwise_matches_params = training_config.config['pairwise_matching_params'].copy()
         pairwise_matches_params = FramePairOptions(**pairwise_matches_params)
 
+        # Check that the network used to create the embeddings is consistent
+        if training_config.config['tracker_params']['use_barlow_network']:
+            assert not pairwise_matches_params['use_superglue'], "Cannot set use_superglue=True if the embeddings were created using barlow network"
+
         physical_unit_conversion = PhysicalUnitConversion.load_from_config(cfg)
         pairwise_matches_params.physical_unit_conversion = physical_unit_conversion
 
