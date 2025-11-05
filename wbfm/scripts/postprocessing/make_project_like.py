7ec92e132c10069b46202be4f48f3a339fbbc051
Charles-Fieseler-Vienna <charles.fieseler@univie.ac.at>
2025-11-05 11:31:02 +0100

    Remove unused args

--- wbfm/scripts/postprocessing/make_project_like.py
+++ wbfm/scripts/postprocessing/make_project_like.py
@@ -8,7 +8,7 @@ from wbfm.utils.projects.project_config_classes import make_project_like
 
 # Initialize sacred experiment
 ex = Experiment(save_git_info=False)
-ex.add_config(project_path=None, target_directory=None, target_suffix=None, steps_to_keep=None, config_files_to_update=None, DEBUG=False)
+ex.add_config(project_path=None, target_directory=None, target_suffix=None, steps_to_keep=None, DEBUG=False)
 
 
 @ex.automain
@@ -19,7 +19,6 @@ def main(_config, _run):
     project_path = _config['project_path']
     steps_to_keep = _config['steps_to_keep']
     target_suffix = _config['target_suffix']
-    config_files_to_update = _config['config_files_to_update']
 
     make_project_like(project_path, target_directory=target_directory,
-                      target_suffix=target_suffix, steps_to_keep=steps_to_keep, config_files_to_update=config_files_to_update, verbose=1)
+                      target_suffix=target_suffix, steps_to_keep=steps_to_keep, verbose=1)
